723633285ea047c00fe81288bdc5e2a5b489c85d
Pavel Popov <pavel.popov@ispring.com>
2025-11-12 23:34:45 +0300

    Обновил функцию скрытия попапов

--- src/common/tools/skipPopups.ts
+++ src/common/tools/skipPopups.ts
@@ -1,7 +1,16 @@
-async function skipPopups(browser: WebdriverIO.Browser, userID: string): Promise<void> {
+async function skipPopups(
+    browser: WebdriverIO.Browser, 
+    userID: string = 'e8bafd26-54cb-11ee-9701-e2c1a9365852',
+): Promise<void> {
     await browser.execute(ID => {
         localStorage.setItem(`ispring::learn::coursesPage::welcomePopup::${ID}`, '1')
+        localStorage.setItem(`ispring::learn::homePage::welcomePopup::${ID}`, '1')
     }, userID)
+
+    await browser.setCookies( { 
+        name: 'is_cookie_policy_accepted',
+        value: 'accepted',
+     })  
 }
 
 export {
